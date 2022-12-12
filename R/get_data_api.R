b438a740ff481b609f9b77d227893bddb9693ea9
facarranza <fcarranza@protonmail.com>
2022-12-12 17:20:12 -0500

    updata: get data  APi appears}

--- R/get_data_api.R
+++ R/get_data_api.R
@@ -14,7 +14,7 @@ get_data_api <- function(name,selector=NULL){
     request <- airtabler::airtable(base = "app2THgdU5NHqq1hh" , #appxxxxxxxx
                                    tables = c("tbl0RZB8txg6xaDVZ")#eg: ("tabla1", "tabla2")
     )
-    request= as.data.frame(request$tbl0RZB8txg6xaDVZ$select())
+    request <- as.data.frame(request$tbl0RZB8txg6xaDVZ$select())
     request$Request..doc.=""
     list_temp2=""
 
@@ -23,7 +23,7 @@ get_data_api <- function(name,selector=NULL){
       # print(nrow(request$`Request (doc)`[[i]][3]))
       if(!is.null(nrow(request$`Request (doc)`[[i]][3]))){
         for(j in 1:nrow(request$`Request (doc)`[[i]][3])){
-          temp=paste(temp, a$`Request (doc)`[[i]][j,3] , paste("(",a$`Request (doc)`[[i]][j,2],")",sep=""),sep=" ")
+          temp=paste(temp, request$`Request (doc)`[[i]][j,3] , paste("(",request$`Request (doc)`[[i]][j,2],")",sep=""),sep=" ")
         }
         request$Request..doc.[i]=temp
         # print(temp)
@@ -31,21 +31,47 @@ get_data_api <- function(name,selector=NULL){
     }
 
     b <- as.data.frame(countries$tbl6WB3ZHH9d7ZicJ$select())
-    request$Country = unlist(request$Country)
-    request=request %>% left_join(b,by=c("Country"="id"))
-    request=request %>% rename(country_id=Country, Country= Country.y)
+    request <- Country = unlist(request$Country)
+    request <- request %>% left_join(b,by=c("Country"="id"))
+    request <- request %>% rename(country_id=Country, Country= Country.y)
     print(result)
-    request = request |> select(!c(country_id,`Request (doc)`))
+    request <- request |> select(!c(country_id,`Request (doc)`))
 
     # load("data/request.rda") #csv version
-    result=request
+    result <- request
     rm(request)
   }
 
   if(name=="appeals"){
+    appeals <- airtabler::airtable(base = "app2THgdU5NHqq1hh" , #appxxxxxxxx
+                                   tables = c("tblAEExG4kBQao3uC")#eg: ("tabla1", "tabla2")
+    )
 
-    load("data/appeals.rda")
-    result=appeals
+
+    b <- as.data.frame(countries$tbl6WB3ZHH9d7ZicJ$select())
+    a <- as.data.frame(appeals$tblAEExG4kBQao3uC$select())
+
+
+    a$Attachments_text <- ""
+    list_temp2 <- ""
+    for(i in 1:nrow(a)){
+      temp <- ""
+      print(nrow(a$Attachments[[i]][3]))
+      if(!is.null(nrow(a$Attachments[[i]][3]))){
+        for(j in 1:nrow(a$Attachments[[i]][3])){
+          temp <- paste(temp, a$Attachments[[i]][j,3] , paste("(",a$Attachments[[i]][j,2],")",sep=""),sep=" ")
+        }
+        a$Attachments_text[i] <- temp
+      }
+    }
+
+    a$Country = unlist(a$Country)
+    a <- a %>% left_join(b,by=c("Country"="id"))
+    a <- a %>% rename(country_id=Country, Country= Country.y)
+    a <- a %>% rename(Attachments_list=Attachments, Attachments= Attachments_text)
+    a <- a |> select(!c(country_id,Attachments_list))
+    # load("data/appeals.rda")
+    result <- a
     rm(appeals)
   }
 
